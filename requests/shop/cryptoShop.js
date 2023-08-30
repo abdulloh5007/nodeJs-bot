fab4986d2e24a9a57882fee1d60ad283126c281b
abdulloh5007 <abdulloh50007@gmail.com>
2023-08-30 12:30:29 +0500

    v 1.7 release upd crypto adding cars

--- requests/shop/cryptoShop.js
+++ requests/shop/cryptoShop.js
@@ -1,8 +1,10 @@
 const { donatedUsers } = require("../donate/donatedUsers")
+const { formatNumberInScientificNotation } = require("../systems/systemRu")
 
 let globalAmountCrypto
 let globalNameCrypto
 let globalCountCrypto
+let globalPermission = true
 
 async function buyCryptoCurrence(msg, bot, collection, collectionCrypto) {
     const text = msg.text
@@ -31,6 +33,12 @@ async function buyCryptoCurrence(msg, bot, collection, collectionCrypto) {
                 const cryptoPrice = crypto.price
                 const cryptoStatus = crypto.status
                 const amountCrypto = cryptoPrice * parts[3]
+                if (isNaN(parts[3])) {
+                    bot.sendMessage(chatId, `
+${userDonateStatus}, Вы не можете купить количество букв
+                    `, { parse_mode: 'HTML', reply_to_message_id: messageId })
+                    return;
+                }
                 if (parts[3] <= 0) {
                     bot.sendMessage(chatId, `
 ${userDonateStatus}, Не возможно купить отрицательное или нолевое количество крипто валют
@@ -54,6 +62,7 @@ ${userDonateStatus}, Не возможно купить отрицательно
                     discount = `${globalAmountCrypto} 5%`
                 }
                 else {
+                    globalAmountCrypto = amountCrypto
                     discount = `${amountCrypto}`
                 }
 
@@ -62,11 +71,137 @@ ${userDonateStatus}, Не возможно купить отрицательно
                     globalCountCrypto = parts[3]
 
                     bot.sendMessage(chatId, `
-${userDonateStatus}, Подтвердите оплату с нажав на кнопку ниже
+${userDonateStatus}, Подтвердите оплату нажав на кнопку ниже
 Имя: ${crypto.name}
 Кол-во: ${parts[3]}
 Стоимость: ${discount}
-                    `, { parse_mode: 'HTML', reply_to_message_id: messageId, ...payButton })
+
+После 6 секунд сообщение удалиться !
+                    `, { parse_mode: 'HTML', reply_to_message_id: messageId, ...payButton }).then((message) => {
+                        let againBuy = {
+                            reply_markup: {
+                                inline_keyboard: [
+                                    [{ text: 'Купить', switch_inline_query_current_chat: `купить крипту ${globalNameCrypto} [кол-во]` }]
+                                ]
+                            }
+                        }
+                        
+                        setTimeout(() => {
+                            if (globalPermission === false) {
+                                return;
+                            }
+                            bot.editMessageText(`
+${userDonateStatus}, Вы не успели купить
+
+Нажмите кнопку ниже чтобы купить еще раз
+                            `, {
+                                chat_id: chatId,
+                                message_id: message.message_id,
+                                ...againBuy,
+                                parse_mode: 'HTML',
+                            })
+                        }, 6000);
+                    })
+                }
+                else {
+                    bot.sendMessage(chatId, `
+${userDonateStatus}, Это криптовалюта еще не в продаже
+                    `, { parse_mode: 'HTML', reply_to_message_id: messageId })
+                }
+            }
+            else {
+                bot.sendMessage(chatId, `
+${userDonateStatus}, Данное введенно имя крипто валюты не существует
+Напишите: <code>магазин</code> - чтобы узнать существующие крипто валюты
+                `, { parse_mode: 'HTML', reply_to_message_id: messageId })
+            }
+        }
+        else {
+            bot.sendMessage(chatId, `
+${userDonateStatus}, не правильно введена команда, 
+Пример <code>купить крипту [имя] [кол-во]</code>
+            `, { parse_mode: 'HTML', reply_to_message_id: messageId })
+        }
+    }
+
+    if (parts[0] === '@levouJS_bot' && parts[1].toLowerCase() === 'купить' && parts[2].toLowerCase() === 'крипту') {
+        if (parts.length === 5) {
+            const crypto = await collectionCrypto.findOne({ name: parts[3] })
+            if (!!crypto) {
+                const cryptoPrice = crypto.price
+                const cryptoStatus = crypto.status
+                const amountCrypto = cryptoPrice * parts[4]
+
+                if (isNaN(parts[4])) {
+                    bot.sendMessage(chatId, `
+${userDonateStatus}, Вы не можете купить количество букв
+                    `, { parse_mode: 'HTML', reply_to_message_id: messageId })
+                    return;
+                }
+
+                if (parts[4] <= 0) {
+                    bot.sendMessage(chatId, `
+${userDonateStatus}, Не возможно купить отрицательное или нулевое количество крипто валют
+                    `, { parse_mode: 'HTML', reply_to_message_id: messageId })
+                    return;
+                }
+
+                if (userStatusName === 'premium') {
+                    const discountedVal = Math.floor((amountCrypto * 10) / 100)
+                    globalAmountCrypto = amountCrypto - discountedVal
+                    discount = `${globalAmountCrypto} с 10% скидкой`
+                }
+                else if (userStatusName === 'vip') {
+                    const discountedVal = Math.floor((amountCrypto * 7) / 100)
+                    globalAmountCrypto = amountCrypto - discountedVal
+                    discount = `${globalAmountCrypto} с 7% скидкой`
+                }
+                else if (userStatusName === 'standart') {
+                    const discountedVal = Math.floor((amountCrypto * 5) / 100)
+                    globalAmountCrypto = amountCrypto - discountedVal
+                    discount = `${globalAmountCrypto} с 5% скидкой`
+                }
+                else {
+                    globalAmountCrypto = amountCrypto
+                    discount = `${amountCrypto}`
+                }
+
+                if (cryptoStatus === true) {
+                    globalNameCrypto = parts[2]
+                    globalCountCrypto = parts[3]
+
+                    bot.sendMessage(chatId, `
+${userDonateStatus}, Подтвердите оплату нажав на кнопку ниже
+Имя: ${crypto.name}
+Кол-во: ${parts[4]}
+Стоимость: ${discount.toLocaleString('de-DE')} ${formatNumberInScientificNotation(discount)}
+
+После 6 секунд сообщение удалиться !
+                    `, { parse_mode: 'HTML', reply_to_message_id: messageId, ...payButton }).then((message) => {
+                        let againBuy = {
+                            reply_markup: {
+                                inline_keyboard: [
+                                    [{ text: 'Купить', switch_inline_query_current_chat: `купить крипту ${globalNameCrypto} [кол-во]` }]
+                                ]
+                            }
+                        }
+                        
+                        setTimeout(() => {
+                            if (globalPermission === false) {
+                                return;
+                            }
+                            bot.editMessageText(`
+${userDonateStatus}, Вы не успели купить
+
+Нажмите кнопку ниже чтобы купить еще раз
+                            `, {
+                                chat_id: chatId,
+                                message_id: message.message_id,
+                                ...againBuy,
+                                parse_mode: 'HTML',
+                            })
+                        }, 6000);
+                    })
                 }
                 else {
                     bot.sendMessage(chatId, `
@@ -105,29 +240,31 @@ async function payTransactions(msg, bot, collection) {
     if (payment === 'paymentCrypto') {
         if (userId1 != userToPayment) {
             bot.answerCallbackQuery(msg.id, { show_alert: true, text: 'Это кнопка не для тебя' })
+            globalPermission = true
             return;
         }
-        
+
         if (userCardBalance < globalAmountCrypto) {
-            bot.answerCallbackQuery(msg.id, { show_alert: true, text: 'У вас не хватает средств на <b>балансе КАРТЫ !</b>', parse_mode: 'HTML' })
+            bot.answerCallbackQuery(msg.id, { show_alert: true, text: 'У вас не хватает средств на балансе КАРТЫ !' })
+            globalPermission = true
+            return;
         }
-        else {
-            bot.editMessageText(`
+
+        bot.editMessageText(`
 ${userDonateStatus}, Вы успешно оплатили
 
 Имя валюты: ${globalNameCrypto}
 Кол-во: ${globalCountCrypto}
         `, {
-                chat_id: chatId,
-                message_id: messageId,
-                parse_mode: 'HTML'
-            })
-            const updateObject = {};
-            updateObject[`crypto.0.${globalNameCrypto}`] = parseInt(globalCountCrypto);
-
-            collection.updateOne({ id: parseInt(userToPayment) }, { $inc: updateObject })
-            collection.updateOne({ id: parseInt(userToPayment) }, { $inc: { "bankCard.0.cardValue": -globalAmountCrypto } })
-        }
+            chat_id: chatId,
+            message_id: messageId,
+            parse_mode: 'HTML'
+        })
+        const updateObject = {};
+        updateObject[`crypto.0.${globalNameCrypto}`] = parseInt(globalCountCrypto);
+        collection.updateOne({ id: parseInt(userToPayment) }, { $inc: updateObject })
+        collection.updateOne({ id: parseInt(userToPayment) }, { $inc: { "bankCard.0.cardValue": -parseInt(globalAmountCrypto) } })
+        globalPermission = false
     }
 }
 
